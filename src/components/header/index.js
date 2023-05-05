103039b75d9d7ba99c176a70a43b5a76e76cdb34
aminnicevic04 <aminnicevic@gmail.com>
2023-05-05 08:12:21 +0200

    components

---
+++ src/components/header/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>LOGO</h1>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/main/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./main.css";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="card1">
+        <p>
+          React is the most popular JavaScript front-end framework in use today.
+          It’s used by both established companies and new startups. React was
+          released by Facebook in 2013, and they still use it today for many of
+          their applications. If you’re new to React, this article will help
+          introduce you to the basics. Or, if you’d rather learn by doing, you
+          can jump right in with our Learn React course
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
