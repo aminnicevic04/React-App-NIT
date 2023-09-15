5f519bb7ecf6e6bf1055a538754cf7dcfb882e64
aminnicevic04 <aminnicevic@gmail.com>
2023-09-15 18:34:43 +0200

    cas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,37 @@ import Card from "./components/Card";
 
 //JSX
 function App() {
+  const odeljenje = [
+    {
+      ime: "amin nicevic",
+      godine: 2004,
+      obrazovanje: "DUNP",
+      zanimanje: "React",
+    },
+    {
+      ime: "hasan mujanovic",
+      godine: 2005,
+      obrazovanje: "DUNP",
+      zanimanje: "Angular",
+    },
+  ];
   return (
     <div className="mainDiv">
-      <Card></Card>
+      {/* <Card ime={"Amin Nicevic"} obrazovanje={"DUNP"} />
+      <Card ime={"Jedan drugi decko"} obrazovanje={"DUNP"} /> */}
+      {/* <Card /> */}
+      {/* <Card ime={}/> */}
+      {odeljenje.map((el) => {
+        if (el.godine > 18) {
+          return (
+            <Card
+              ime={el.ime}
+              obrazovanje={el.obrazovanje}
+              godine={el.godine}
+            />
+          );
+        }
+      })}
     </div>
   );
 }
--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,17 +1,50 @@
 import React from "react";
 import "./Card.css";
+import slika from "./amci2.jpg";
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 
-function Card() {
+function Card(props) {
+  const { ime, obrazovanje } = props;
+
+  console.log(ime, obrazovanje);
   return (
-    <div className="kartica">
-      <h1>Amin Nicevic</h1>
-      <p>React Developer</p>
-      <div className="describe">
-        <p>+381291923</p>
-        <p>linkedIn: Amin Nicevic</p>
-        <p>github: Amin Nicevic</p>
+    <>
+      <div className="slika1">
+        <img src={slika} alt="amci" />
+        <div className="prvitekst">
+          <p>{props.ime}</p>
+        </div>
+        <div className="drugitekst">{props.obrazovanje}</div>
+        <div className="drugitekst">{props.godine}</div>
+        <div className="wcp">
+          <a
+            href="https://github.com/aminnicevic04"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={32} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/amin-nicevic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={32} />
+          </a>
+          <a
+            href="https://www.instagram.com/amin.nicevic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram size={32} />
+          </a>
+        </div>
       </div>
-    </div>
+
+      <div className="container">
+        <div className="centarkontent"></div>
+      </div>
+    </>
   );
 }
 
