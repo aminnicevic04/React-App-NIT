d60bdffaf6fe35b49529b2135fa06e1088b1e885
aminnicevic04 <aminnicevic@gmail.com>
2023-10-02 14:56:55 +0200

    domaci

--- my-app/src/components/Cond.Rend-domaci/main.js
+++ my-app/src/components/Cond.Rend-domaci/main.js
@@ -5,29 +5,31 @@ function Main() {
   const [pozadina, setPozadina] = useState("");
 
   const generateRandomNum = () => {
-    const zima =
-      "https://1.bp.blogspot.com/-oJOC5GCA5Bs/WG6bIACd1JI/AAAAAAAAAEE/hq4oGpMOblUhTo0kC6TdAqsfx-z96Fz6wCLcB/s1600/zima1.jpg";
-    const prolece =
-      "https://naissus.info/wp-content/uploads/2019/03/Prole%C4%87e-ilustracija-750x430-696x399.jpg";
-    const leto =
-      "https://seekingmagazine.com/wp-content/uploads/2021/06/opt__aboutcom__coeus__resources__content_migration__mnn__images__2018__07__palm_trees_hot_sun-f8e20b86425b492f9d777d92db46db49.jpg";
-
     const random = Math.floor(Math.random() * (46 - -38 + 1)) + -38;
     setRandomNum(random);
 
     if (random >= -38 && random < -5) {
-      setPozadina(zima);
+      setPozadina(
+        "https://1.bp.blogspot.com/-oJOC5GCA5Bs/WG6bIACd1JI/AAAAAAAAAEE/hq4oGpMOblUhTo0kC6TdAqsfx-z96Fz6wCLcB/s1600/zima1.jpg"
+      );
     } else if (random >= -5 && random < 20) {
-      setPozadina(prolece);
+      setPozadina(
+        "https://naissus.info/wp-content/uploads/2019/03/Prole%C4%87e-ilustracija-750x430-696x399.jpg"
+      );
     } else {
-      setPozadina(leto);
+      setPozadina(
+        "https://seekingmagazine.com/wp-content/uploads/2021/06/opt__aboutcom__coeus__resources__content_migration__mnn__images__2018__07__palm_trees_hot_sun-f8e20b86425b492f9d777d92db46db49.jpg"
+      );
     }
   };
 
   const styles = {
-    background: pozadina,
+    backgroundImage: `url(${pozadina})`,
     color: "white",
+    backgroundSize: "cover",
   };
+
+  console.log(styles);
 
   return (
     <div className="main" style={styles}>
