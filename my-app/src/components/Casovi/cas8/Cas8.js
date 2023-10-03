166cb32604de02970a6ffaa4b4e3e9ed62d4a3e0
aminnicevic04 <aminnicevic@gmail.com>
2023-10-03 10:45:44 +0200

    cas update

--- my-app/src/components/Casovi/cas8/Cas8.js
+++ my-app/src/components/Casovi/cas8/Cas8.js
@@ -1,13 +1,44 @@
 function Cas8() {
-  const getData = () => {
+  const [defaultData, setDefaultData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getData = () =>
     fetch("https://dummyjson.com/products")
       .then((res) => res.json())
-      .then(console.log);
-  };
+      .then((data) => setDefaultData(data?.products));
 
+  console.log(defaultData);
   return (
-    <div>
-      <button></button>
+    <div style={{ minHeight: "100vh" }}>
+      <button onClick={getData}>Uzmi podatke</button>
+      <button
+        onClick={() =>
+          setFilteredData(defaultData.filter((el) => el.id % 2 === 0))
+        }
+      >
+        Filtriraj podatke
+      </button>
+      x
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          height: "200px",
+          width: "100px",
+        }}
+      >
+        {loading && <h1>Loading.....</h1>}
+        {defaultData?.length > 0 ? (
+          defaultData?.map((product) => (
+            <h1 style={{ border: "1px solid black" }} key={product.id}>
+              {product.id}
+            </h1>
+          ))
+        ) : (
+          <h1>Nema trenutno podataka </h1>
+        )}
+      </div>
     </div>
   );
 }
