81cd019188e47f3c65fb3ea69ea6bbd8b7335119
aminnicevic04 <aminnicevic@gmail.com>
2023-10-09 17:49:40 +0200

    finish

--- my-app/src/components/Search.Bar-doamci/Search.js
+++ my-app/src/components/Search.Bar-doamci/Search.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 import SearchBar from "./comp/SearchBar";
+import SearchResultsList from "./comp/SearchResultsList";
 
 function Search() {
+  const [results, setResults] = useState([]);
+
   return (
     <div className="App">
       <div className="search-bar">
-        <SearchBar />
-        <div>search result</div>
+        <SearchBar setResults={setResults} />
+        <SearchResultsList results={results} />
       </div>
     </div>
   );
