540f0629c04f5cd1e5720ad3bbe0e81c63cfdab5
aminnicevic04 <aminnicevic@gmail.com>
2023-10-11 18:27:14 +0200

    axios update

--- my-app/src/components/Casovi/cas11/cas11.js
+++ my-app/src/components/Casovi/cas11/cas11.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
 import KarticaDomaci from "./KarticaDomaci";
 import "./style.css";
+import axios from "axios";
 
 function Cas11() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
   const [category, setCategory] = useState("");
+  const BASE_URL = "https://dummyjson.com/products";
 
-  const getData = () => {
-    fetch(`https://dummyjson.com/products/search?q=${search}`)
-      .then((res) => res.json())
-      .then((products) => setData(products.products))
-      .catch((error) => alert("Error fetching data:", error));
+  // const getData = () => {
+  //   fetch(`https://dummyjson.com/products/search?q=${search}`)
+  //     .then((res) => res.json())
+  //     .then((products) => setData(products.products))
+  //     .catch((error) => alert("Error fetching data:", error));
+  // };
+
+  const getData2 = () => {
+    axios
+      .get(`${BASE_URL}/search?q=${search}`)
+      .then((res) => setData(res.data.products));
   };
 
   const getDataByCategory = () => {
@@ -21,11 +29,15 @@ function Cas11() {
       .catch((error) => alert("Error fetching data:", error));
   };
 
+  const getCategories = () => {
+    axios.get(`${BASE_URL}/categories`);
+  };
+
   useEffect(() => {
     if (category) {
       getDataByCategory();
     } else {
-      getData();
+      getData2();
     }
   }, [search, category]);
 
